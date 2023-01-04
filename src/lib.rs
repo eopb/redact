9e69e1fe9dd0d61bf6a9f93c348e460ea5841482
Ethan Brierley <ethanboxx@gmail.com>
2023-01-04 18:42:53 +0000

    Simplify

--- src/lib.rs
+++ src/lib.rs
@@ -15,37 +15,25 @@ use serde::{Deserialize, Serialize};
 /// let encryption_key: Secret<&str> = Secret::new("hello world");
 /// assert_eq!("hello world", *encryption_key.expose_secret())
 /// ```
-/// ```
-/// use redact::Secret;
-/// let encryption_key: Secret<&str, false> = Secret::new("hello world");
-/// assert_eq!("[REDACTED]", format!("{encryption_key:?}"))
-/// ```
 #[derive(Serialize, Deserialize, Default, Copy, Clone, Eq, PartialEq)]
 #[serde(transparent)]
-pub struct Secret<T, const DISPLAY_TYPE_NAME: bool = true>(T);
+pub struct Secret<T>(T);
 
-impl<T, const DISPLAY_TYPE_NAME: bool> Secret<T, DISPLAY_TYPE_NAME> {
+impl<T> Secret<T> {
     #[inline]
     pub fn new(secret: T) -> Self {
         Self(secret)
     }
 }
 
-impl<T, const DISPLAY_TYPE_NAME: bool> From<T> for Secret<T, DISPLAY_TYPE_NAME> {
+impl<T> From<T> for Secret<T> {
     #[inline]
     fn from(secret: T) -> Self {
         Self::new(secret)
     }
 }
 
-impl<T> fmt::Debug for Secret<T, false> {
-    #[inline]
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "[REDACTED]")
-    }
-}
-
-impl<T> fmt::Debug for Secret<T, true> {
+impl<T> fmt::Debug for Secret<T> {
     #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "[REDACTED {}]", type_name::<T>())
@@ -60,10 +48,10 @@ pub trait ExposeSecret: Sealed {
 use private::Sealed;
 mod private {
     pub trait Sealed {}
-    impl<T, const DISPLAY_TYPE_NAME: bool> Sealed for super::Secret<T, DISPLAY_TYPE_NAME> {}
+    impl<T> Sealed for super::Secret<T> {}
 }
 
-impl<T, const DISPLAY_TYPE_NAME: bool> ExposeSecret for Secret<T, DISPLAY_TYPE_NAME> {
+impl<T> ExposeSecret for Secret<T> {
     type Secret = T;
     #[inline]
     fn expose_secret(&self) -> &Self::Secret {
