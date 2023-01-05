6269365d2803ede4f06433f97b50071e561defb7
Ethan Brierley <ethanboxx@gmail.com>
2023-01-05 08:51:37 +0000

    more See module level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -25,10 +25,12 @@ impl<'de, T: Deserialize<'de>> Deserialize<'de> for Secret<T> {
 }
 
 impl<T> Secret<T> {
+    /// See [module level documentation][crate]
     #[inline]
     pub fn new(secret: T) -> Self {
         Self(secret)
     }
+    /// See [module level documentation][crate]
     #[inline]
     pub fn expose_secret(&self) -> &T {
         &self.0
