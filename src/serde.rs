699dfd77d3718a54623f9ea6d94ff66b8196192a
Ethan Brierley <ethanboxx@gmail.com>
2023-07-16 21:36:29 +0100

    Experiment with more flexiable `expose_secret` that supports `Option`

--- src/serde.rs
+++ src/serde.rs
@@ -10,6 +10,39 @@ impl<'de, T: Deserialize<'de>> Deserialize<'de> for Secret<T> {
     }
 }
 
+#[cfg(feature = "serde")]
+#[derive(Serialize)]
+struct Test {
+    #[serde(serialize_with = "expose_secret")]
+    one: Secret<String>,
+    #[serde(serialize_with = "expose_secret")]
+    two: Option<Secret<String>>,
+}
+
+pub trait ContainsSecret<T> {
+    type Exposed<'a>: Serialize
+    where
+        Self: 'a;
+    fn expose_secret<'a>(&'a self, expose: impl Fn(&Secret<T>) -> &T) -> Self::Exposed<'a>;
+}
+
+impl<T: Serialize> ContainsSecret<T> for Secret<T> {
+    type Exposed<'a> = &'a T where T: 'a;
+
+    fn expose_secret<'a>(&'a self, expose: impl Fn(&Secret<T>) -> &T) -> Self::Exposed<'a> {
+        expose(self)
+    }
+}
+
+impl<T: Serialize> ContainsSecret<T> for Option<Secret<T>> {
+    type Exposed<'a> = Option<&'a T> where T: 'a;
+
+    fn expose_secret<'a>(&'a self, expose: impl Fn(&Secret<T>) -> &T) -> Self::Exposed<'a> {
+        self.as_ref().map(expose)
+    }
+}
+
+#[cfg(feature = "serde")]
 /// Exposes a [Secret] for serialization.
 ///
 /// For general-purpose secret exposing see [Secret::expose_secret].
@@ -19,8 +52,10 @@ impl<'de, T: Deserialize<'de>> Deserialize<'de> for Secret<T> {
 /// *This API requires the following crate features to be activated: `serde`*
 #[inline]
 pub fn expose_secret<S: Serializer, T: Serialize>(
-    secret: &Secret<T>,
+    secret: &impl ContainsSecret<T>,
     serializer: S,
 ) -> Result<S::Ok, S::Error> {
-    secret.expose_secret().serialize(serializer)
+    secret
+        .expose_secret(Secret::expose_secret)
+        .serialize(serializer)
 }
