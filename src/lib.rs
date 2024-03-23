b96a2fc1cc780994c6457366c4b074ec373229bc
Ethan Brierley <ethanboxx@gmail.com>
2024-03-23 15:20:34 +0000

    feat: allow secrets to be unsized

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ use core::{any::type_name, fmt, str::FromStr};
 /// See [module level documentation][crate]
 #[derive(Default, Hash, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 #[repr(transparent)]
-pub struct Secret<T>(T);
+pub struct Secret<T: ?Sized>(T);
 
 impl<T> Secret<T> {
     /// See [module level documentation][crate]
